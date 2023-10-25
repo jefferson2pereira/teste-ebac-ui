3b217745d4bb9d16d517387242bf56675e5b5ff1
Jefferson Pereira <jefferson2pereira@hotmail.com>
2023-10-25 17:28:10 -0300

    usando massa de dados

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,40 +1,58 @@
 /// <reference types="cypress" />
+const Perfil = require('../fixtures/perfil.json')
 
-context('funcionalidade login',() =>{
-    
+context('funcionalidade login', () => {
+
     beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-});
+        cy.visit('minha-conta')
+    });
     afterEach(() => {
-    cy.screenshot()
-});
+        cy.screenshot()
+    });
 
-    it('Deve fazer login com sucesso', () =>{
-    
-    cy.get('#username').type('aluno_ebac@teste.com')
-    cy.get('#password').type('teste@teste.com')
-    cy.get('.woocommerce-form > .button').click()
+    it('Deve fazer login com sucesso', () => {
 
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac20')
-    
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac20')
+
     })
 
-    it('Deve emitir uma mensagem de erro ao inserir usuário inválido', () =>{
-        
-    cy.get('#username').type('ebac@teste.com')
-    cy.get('#password').type('teste@teste.com')
-    cy.get('.woocommerce-form > .button').click()
+    it('Deve fazer login com sucesso- Usando banco de dados', () => {
 
-    cy.get('.woocommerce-error > li').should('contain','Erro')
+        cy.get('#username').type(Perfil.usuario)
+        cy.get('#password').type(Perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
     })
 
-    it('Deve emitir uma mensagem de erro ao inserir senha inválida', () =>{
-        
-    cy.get('#username').type('aluno_ebac@teste.com')
-    cy.get('#password').type('teste@teste')
-    cy.get('.woocommerce-form > .button').click()
+    it('Deve fazer login com sucesso- Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
 
-    cy.get('.woocommerce-error > li').should('contain','Erro')
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha,{log: false})
+            cy.get('.woocommerce-form > .button').click()
+
+        })
+    })
+
+    it('Deve emitir uma mensagem de erro ao inserir usuário inválido', () => {
+
+        cy.get('#username').type('ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Erro')
+    })
+
+    it('Deve emitir uma mensagem de erro ao inserir senha inválida', () => {
+
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Erro')
     })
 
 })
